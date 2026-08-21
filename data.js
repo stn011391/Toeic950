cc61f295717fad8009d540a527e2dd1f097bcad6
stn011391 <stn011391@gmail.com>
2026-08-21 08:18:21 +0800

    load v1.5 generated-language quality pass

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var QUESTION_COUNT=0;
 if(typeof document!=='undefined'){
-  document.write('<script src="data-diversity-v2.js"><\/script><script src="data-diversity-contexts.js"><\/script><script src="data-diversity-cleanup.js"><\/script><script src="data-finalize.js"><\/script>');
+  document.write('<script src="data-diversity-v2.js"><\/script><script src="data-diversity-contexts.js"><\/script><script src="data-quality-fixes.js"><\/script><script src="data-diversity-cleanup.js"><\/script><script src="data-finalize.js"><\/script>');
 }else{
   QUESTION_COUNT=Object.values(QUESTIONS).reduce((n,a)=>n+a.length,0);
 }
