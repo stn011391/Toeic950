1e707b2cce90032f31747d26699acec2c057664d
stn011391 <stn011391@gmail.com>
2026-08-21 08:41:20 +0800

    v1.5.0 cache-bust generated data modules

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 var QUESTION_COUNT=0;
 if(typeof document!=='undefined'){
-  document.write('<script src="data-diversity-v2.js"><\/script><script src="data-diversity-contexts.js"><\/script><script src="data-quality-fixes.js"><\/script><script src="data-diversity-cleanup.js"><\/script><script src="data-finalize.js"><\/script>');
+  const v=encodeURIComponent(typeof APP_VERSION!=='undefined'?APP_VERSION:'v1.5.0');
+  document.write('<script src="data-diversity-v2.js?v='+v+'"><\/script><script src="data-diversity-contexts.js?v='+v+'"><\/script><script src="data-quality-fixes.js?v='+v+'"><\/script><script src="data-diversity-cleanup.js?v='+v+'"><\/script><script src="data-finalize.js?v='+v+'"><\/script>');
 }else{
   QUESTION_COUNT=Object.values(QUESTIONS).reduce((n,a)=>n+a.length,0);
 }
